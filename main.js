c47feabdd9e7f39bfb6174af5969449a719e202a
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-05-03 21:25:17 +0300

    adding  a menu

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
 // starting the project
 
 $(window).on("load", function () {
+  buttons();
   page2Hider();
   $(`#mainContainerDiv`).hide();
 });
 const list_body_h2 = $(`#list-viewer`);
 list_body_h2.html(` <ol id="list_items"></ol>`);
 
+
+
+
+
+// under this comment a declration of all buttons and a function contains there css.
 const addBtn = $(`#btn-add`);
+const deleteButton = $(`#deletBtn`);
+const addToDoBtn = $(`#btn-add-ToDo`);
+const homeBtn = $(`#home`);
+const home1Btn = $(`#home1`);
+buttons=(()=>{
+
+
 addBtn.html(`<svg xmlns="http://www.w3.org/2000/svg" width="36" height="40" fill="green" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
 <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
 <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
 </svg>`);
 
-const deleteButton = $(`#deletBtn`);
+
 deleteButton.html(`<svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="#EB3636" class="bi bi-trash-fill" viewBox="0 0 16 16">
 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
 </svg></button>`);
 
-const addToDoBtn = $(`#btn-add-ToDo`);
+
 addToDoBtn.html(`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-plus-square" viewBox="0 1 16 13">
 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
 </svg>`);
 
-const homeBtn = $(`#home`);
+
 homeBtn.html(`<svg xmlns="http://www.w3.org/2000/svg" width="36" height="40" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 </svg>`);
+
+home1Btn.html(`<svg xmlns="http://www.w3.org/2000/svg" width="36" height="40" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+<path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+<path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+</svg>`);
+
+})
 
 const deletePtow = $(`#btn-delete-ToDo`);
 
@@ -44,9 +64,7 @@ renderList();
 $(`#finishBtn`).click(() => {
   currenStep=1;
   input_value = $(`#list-input`).val();
-  datefunction(()=>{
-    $(`#dateInpput${iValue}`).show();
-  })
+
   if (input_value !== "") {
     addToLists();
     renderList();
@@ -73,7 +91,7 @@ list_item.click(() => {
     "font-size": "30px",
   });
 });
-const iValue=0;
+
 //this function is to render the values(new lists added by user) from the input to the list to view.
 function renderList() {
   let ol = $(`#list_items`);
@@ -84,12 +102,8 @@ function renderList() {
     //I am willing to add delete button in here.
     li = $(`<div class="list-item-class"><li id="list-item-pushed${i}">${listArray[i]}
    
-     </li><input type="date" id="dateInpput${i}" class="Date"><input class="checkBox" type="checkbox" name=checkbox id="deleted${i}"></div>`); // Iam adding an checkBox to delete lists .
+     </li><input class="checkBox" type="checkbox" name=checkbox id="deleted${i}"></div>`); // Iam adding an checkBox to delete lists .
     ol.append(li);
-
-    // localStorage.setItem(`lists`, `listArray`);
-    // localStorage.setItem("listArray", JSON.stringify(listArray))
-    iValue=i;
     $(`#list-item-pushed${i}`).on("click", function () {
       homePageHider();
       $(`#mainContainerDiv`).hide();
@@ -130,12 +144,14 @@ deleteButton.on(`click`, function () {
 });
 
 addBtn.click(() => {
+  
   toListAdder();
 });
 
 toListAdder = () => {
   $(`#Container`).hide();
   $(`#mainContainerDiv`).show();
+  
 };
 
 homePageHider = () => {
@@ -162,13 +178,13 @@ $(`#home`).click(() => {
 const toDoBody = $(`#toDoBody`);
 toDoBody.html(`<ol id="ol-list"></ol>`);
 
-let listToDoArray = [];
+let listToDoArray = JSON.parse(localStorage.getItem("listDoneArray")) || [];
 let input2_value = ``;
 let listDoneArray = [];
 // this button is the one beside input
 $(`#btn-add-ToDo`).click(() => {
   input2_value = $(`#toDo-input`).val();
-
+  
   if (input2_value !== "") {
     addToListsToDo();
     renderListToDo();
@@ -181,6 +197,7 @@ $(`#btn-add-ToDo`).click(() => {
 
 addToListsToDo = function () {
   listToDoArray.push(input2_value);
+  localStorage.setItem("listDoneArray", JSON.stringify(listDoneArray));
 };
 
 //here is to put the user input vlaue from listToDoArray to the ol inside ToDo body.
@@ -200,7 +217,6 @@ renderListToDo = () => {
     $(`#toDo-pushed${i}`).on("click", function () {
       itemId = $(`#toDo-pushed${i}`).attr(`id`);
       itemToDo_Position = i;
-      // console.log(`****************************`);
       deleteFromToDo();
     });
   }
@@ -232,7 +248,7 @@ renderToDone = () => {
   for (let i = 0; i < listDoneArray.length; i++) {
     li_done = $(`<div class="list-item-class"><li id="done-pushed${i}">${listDoneArray[i]}
        
-       </li></div>`);
+       </li><input class="checkBox" type="checkbox" name=checkbox id="deleted${i}"></div>`);
     ol_Done.append(li_done);
 
     $(`#done-pushed${i}`).on("click", function () {
@@ -244,53 +260,26 @@ renderToDone = () => {
 };
 
 deleteFromDone = () => {
-  listDoneArray.splice(itemToDo_Position, 1);
+  
+    // console.log(item_position);
+    listArray.splice(item_position, 1);
+  
+    localStorage.setItem("listDoneArray", JSON.stringify(listDoneArray));
+  
+    renderToDone();
 };
 
 // this section is for the step progress bar.
-const previousBtn = $(`#previousBtn`);
-const nextBtn = $(`#nextBtn`);
+
+
+
 const finishBtn = $(`#finishBtn`);
-const content = $(`#content`);
-const bullets = [...$(`.bullet`)];
-const MAX_STEPS = 3;
-let currenStep = 1;
 
-// here to enable next button only if input has value.
 
-if ($(`#list-input`).val()) nextBtn.attr(`disabled`, true);
-$(`#list-input`).on("change", () => {
-  console.log($(`#list-input`).val());
-  nextBtn.attr(`disabled`, false);
-});
 
-$(`#dateInpput`).hide();
 
-nextBtn.click(() => {
-  const currentBullet = bullets[currenStep - 1];
-  currentBullet.classList.add(`completed`);
 
-  currenStep++;
-  previousBtn.attr(`disabled`, false);
 
-  if (currenStep === MAX_STEPS) {
-    finishBtn.attr(`disabled`, false);
-    nextBtn.attr(`disabled`, true);
-  }
-  $(`#list-input`).hide();
-  
-  datefunction();
-});
 
-previousBtn.click(() => {
-  const previouseBullet = bullets[currenStep - 1];
-  previouseBullet.classList.remove(`completed`);
-  currenStep--;
-  nextBtn.attr(`disabled`, false);
-  finishBtn.attr(`disabled`, true);
-  if (currenStep === 1) {
-    previousBtn.attr(`disabled`, true);
-  }
-  $(`#list-input`).show();
-  $(`#dateInpput`).hide();
-});
+
+
